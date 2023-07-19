5325d2769dc2238cfbe72e525c179d8ff0c08b70
nurayklc <nuraykilic0101@gmail.com>
2023-07-19 11:34:19 +0300

    Add examples

--- FirstProject/src/app/app.component.ts
+++ FirstProject/src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`
+  <h5>Two-Way Data Binding</h5>
+  <input [(ngModel)]="inputText">
+
+  <p>{{ inputText }}</p>
+
+  <p [style.border]="border">Attribute Bindig </p>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirstProject';
+  border:string = "1px solid green";
+  inputText: string = "Hello Angular!";
 }
--- FirstProject/src/app/app.module.ts
+++ FirstProject/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
